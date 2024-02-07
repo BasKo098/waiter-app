544a5281c589ea1793bcdc80543fd0f26ea7aa23
Barbara Kosińska <basia7kosinska@.com>
2024-02-07 12:28:15 +0100

    Add link to button

--- src/components/pages/Home/AllTables/AllTables.js
+++ src/components/pages/Home/AllTables/AllTables.js
@@ -2,9 +2,17 @@
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { Row, Col } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+//import { useSelector } from 'react-redux';
+
+
 
 
 const AllTables = () => {
+
+    //const table = useSelector (state=> getTablebyId(state));
+    //const tables = useSelector (state= getAllTables(state));
+
     return(
         <div>
             <h1>All tables</h1>
@@ -13,8 +21,13 @@ const AllTables = () => {
                     <Col>
                         <h2>Table 1</h2>
                     </Col>
+                    <Col>
+                    <p>Status:</p>
+                    </Col>
                     <Col className="text-end">
-                        <Button variant="primary">Show more</Button>
+                        <NavLink to="/waiter">
+                            <Button variant="primary">Show more</Button>
+                        </NavLink>
                     </Col>
                 </Row>
             </Container>
