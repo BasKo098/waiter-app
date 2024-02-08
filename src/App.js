b63d634bff738617ae24c810a46f7147c89cc884
Barbara Kosińska <basia7kosinska@.com>
2024-02-08 12:37:47 +0100

    Add dispatch FetchTables in App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Footer from './components/views/Footer/Footer';
 import Home from './components/pages/Home/Home';
 import NotFound from './components/pages/NotFound/NotFound';
 import WaiterApp from './components/pages/WaiterApp/WaiterApp';
+import { fetchTables } from './redux/tableReducer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+
+  const dispatch = useDispatch();
+  useEffect(() => {dispatch(fetchTables())}, [dispatch]);
   return (
       <div>
         <Container>
