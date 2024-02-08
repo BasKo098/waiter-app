92ec3c33b1c375d58bd49b3691b3e925faa93614
Barbara Kosińska <basia7kosinska@.com>
2024-02-08 13:12:54 +0100

    Fix routher path

--- src/components/features/AllTables/AllTables.js
+++ src/components/features/AllTables/AllTables.js
@@ -10,36 +10,34 @@ import { useSelector } from 'react-redux';
 const AllTables = () => {
 
     const tables = useSelector(getAllTables);
-    console.log(tables);
 
-
-    return (
-        <div>
-            <h1>All tables</h1>
-            <Container>
-                {tables.map(table => (
-                <Row key={table.id} className="align-items-center">
-                    <Col>
-                        <Row>
-                            <Col>
-                                <h2>Table{table.id}</h2>
-                            </Col>
-                            <Col>
-                                <p className="px-0"></p>
-                                <p><strong>Status:</strong>{table.status}</p>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col className="text-end">
-                        <NavLink to={`/table/${table.id}`}>
-                            <Button variant="primary">Show more</Button>
-                        </NavLink>
-                    </Col>
-                </Row>
-                ))}
-            </Container>
-        </div>
-    )
-};
+        return (
+            <div>
+                <h1>All tables</h1>
+                <Container>
+                    {tables.map(table => (
+                    <Row key={table.id} className="align-items-center">
+                        <Col>
+                            <Row>
+                                <Col>
+                                    <h2>Table{table.id}</h2>
+                                </Col>
+                                <Col>
+                                    <p className="px-0"></p>
+                                    <p><strong>Status:</strong>{table.status}</p>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col className="text-end">
+                            <NavLink to={`/table/${table.id}`}>
+                                <Button variant="primary">Show more</Button>
+                            </NavLink>
+                        </Col>
+                    </Row>
+                    ))}
+                </Container>
+            </div>
+        )
+    };
 
 export default AllTables;
